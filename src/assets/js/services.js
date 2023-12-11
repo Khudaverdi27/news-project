c63fd6e29c0eeb9069fce11d3db30bf9f1da6d03
Xudaverdi <xudaverdi.sh@gmail.com>
2023-12-11 22:30:16 +0400

    categroy section connected with api

--- src/assets/js/services.js
+++ src/assets/js/services.js
@@ -1,5 +1,11 @@
 import { get } from './request.js';
 export const serviceNewsList = async () => {
     const res = await get('/news')
-    return res
+    return res.data
+}
+
+export const serviceCategoryList = async () => {
+    const category = await get('/news/category')
+    return category
+
 }
--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -1,4 +1,4 @@
-import { serviceNewsList } from "./services.js"
+import { serviceNewsList, serviceCategoryList } from "./services.js"
 
 export const getUiTemplate = (id, selector) => {
     const element = document.getElementById(id)
@@ -7,10 +7,36 @@ export const getUiTemplate = (id, selector) => {
     return clone.querySelector(selector)
 }
 
-export const uiNavigator = () => {
+const getCategoryIcon = (key) => {
+    const icons = {
+        "world": 'icon icon-globe1',
+        "politics": 'icon icon icon-slack1',
+        "sports": ' icon icon-soccer-ball-o',
+        "technology": 'icon icon-smartphone',
+        "economy": 'icon icon-pie-chart',
+        "entertainment": 'icon icon-film1',
+        "health": 'icon icon-activity',
+        "science": 'icon icon-newspaper-o',
+        "culture": 'icon icon-slideshare',
+        "environment": 'icon icon-ge'
+    }
+    return icons[key]
+}
+
+export const uiNavigator = async () => {
+
+    const category = await serviceCategoryList()
     const navContainer = document.getElementById('navigation')
     const tagLink = getUiTemplate('navigation-item', '.nav-link')
-    navContainer.innerHTML = tagLink.outerHTML
+    let html = ''
+
+    category.forEach(c => {
+        let newtemplate = tagLink
+        newtemplate.querySelector('i').classList = `${getCategoryIcon(c.slug)}`
+        newtemplate.querySelector('.category-head').textContent = c.name
+        html += newtemplate.outerHTML
+    });
+    navContainer.innerHTML = html
 }
 
 export const uiSubscription = () => {
@@ -20,9 +46,19 @@ export const uiSubscription = () => {
 
 }
 export const uiNews = async () => {
-    const res = serviceNewsList()
+    const res = await serviceNewsList()
     const content = document.getElementById('news-content')
     const template = getUiTemplate('news-template', 'article')
-    content.innerHTML = template.outerHTML
 
+    let html = ''
+
+    res.forEach(i => {
+        let newtemplate = template
+        newtemplate.querySelector('figure img').src = i.photo
+        newtemplate.querySelector('.title').textContent = i.title
+        newtemplate.querySelector('.description').textContent = i.description
+        newtemplate.querySelector('.agency').textContent = i.author.agency
+        html += template.outerHTML
+    });
+    content.innerHTML = html
 }
